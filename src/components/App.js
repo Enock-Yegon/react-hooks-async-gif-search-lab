11757a517f7b2bdb6dc80f11ef2b4e1c7da25bfd
Enock-Yegon <ykibet70@gmail.com>
2023-01-24 10:07:40 +0300

    Completed Assignment

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,15 @@ import React from "react";
 
 import NavBar from "./NavBar";
 
+import GiftListContainer from "./GiftListContainer";
+
 // the App component should render out the GifListContainer component
 
 function App() {
   return (
     <div>
       <NavBar color="black" title="Giphy Search" />
+      <GiftListContainer />
     </div>
   );
 }
